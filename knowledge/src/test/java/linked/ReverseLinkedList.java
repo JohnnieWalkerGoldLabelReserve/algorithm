56f3f765d258855e6ceccda1ed38e37bcc1a4b46
zhaoyang56 <zhaoyang56@meituan.com>
2022-11-04 19:33:58 +0800

    add

--- knowledge/src/test/java/linked/ReverseLinkedList.java
+++ knowledge/src/test/java/linked/ReverseLinkedList.java
@@ -35,4 +35,53 @@ public class ReverseLinkedList {
         return pre;
     }
 
+    public ListNode reverseListParatation(ListNode head, int l, int r) {
+        if (head == null || head.next == null || l == r) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode start = null;
+        ListNode firstNode = null;
+        int ans = 1;
+        for (ListNode cur = head; cur != null; ) {
+            if ((ans + 1) == l) {
+                start = cur;
+            }
+
+            if (ans == l) {
+                ListNode next = cur.next;
+                cur.next = null;
+                firstNode = cur;
+                pre = cur;
+                cur = next;
+                ans++;
+                continue;
+            }
+
+            if (ans == r) {
+                ListNode next = cur.next;
+                if (null != start) {
+                    start.next = cur;
+                }
+                if (null != firstNode) {
+                    firstNode.next = next;
+                }
+                cur.next = pre;
+                break;
+            }
+
+            if (ans < l) {
+                pre =cur;
+                cur = cur.next;
+            } else {
+                ListNode next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+            ans++;
+        }
+        return head;
+    }
+
 }
